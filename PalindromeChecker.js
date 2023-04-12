a30afd8b721033ccc071237351e9b2fd7f879aa2
newby101 <ishimwekevin45@gmail.com>
2023-04-11 20:30:12 -0400

    done

--- PalindromeChecker.js
+++
@@ -1,8 +0,0 @@
-function palindrome(str) {
-  //reverse the key parameter str
-  str = str.replace(/[^0-9a-z]/gi, "").toLowerCase();
-  // Check if the string is equal to its reverse
-  return str === str.split("").reverse().join("");
-}
-
-console.log(palindrome("race car"));
